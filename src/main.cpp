6278eee9ac96b56acc9d544db3ac4d46012f91cf
TITH <1023515576@qq.com>
2023-06-23 17:09:00 +0800

    性能对比 11.4725ms

--- src/main.cpp
+++ src/main.cpp
@@ -28,15 +28,38 @@ public:
     }
 };
 
+double Step1(double input)
+{
+    static double inputs[3], outputs[3];
+    inputs[2] = inputs[1];
+    inputs[1] = inputs[0];
+    inputs[0] = input;
+
+    outputs[2] = outputs[1];
+    outputs[1] = outputs[0];
+
+    outputs[0] = 66.2117333333333 * inputs[0] - 124.136000000000 * inputs[1] + 58.1856000000000 * inputs[2] + 0.333333333333333 * outputs[1] + 0.666666666666667 * outputs[2];
+    return outputs[0];
+}
+
 int main(int, char const **)
 {
     RunTimeStatus run_time_status;
-    DiscreteTf<double> dtf({66.2117333333333,-124.136000000000,58.1856000000000}, {1,-0.333333333333333,-0.666666666666667});
+    DiscreteTf<double> dtf({66.2117333333333, -124.136000000000, 58.1856000000000}, {1, -0.333333333333333, -0.666666666666667});
 
     run_time_status.Start();
     for (size_t i = 0; i < 1000000; i++)
     {
         dtf.Step(1);
+        // Step1(1);
+    }
+    run_time_status.End();
+
+    run_time_status.Start();
+    for (size_t i = 0; i < 1000000; i++)
+    {
+        // dtf.Step(1);
+        Step1(1);
     }
     run_time_status.End();
 
