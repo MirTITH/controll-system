a2fc4dbf41148ce0a98bc5c57a935b9879daa57b
TITH <1023515576@qq.com>
2023-06-26 21:07:05 +0800

    添加注释

--- src/z_tf.hpp
+++ src/z_tf.hpp
@@ -1,9 +1,25 @@
+/**
+ * @file z_tf.hpp
+ * @author X. Y.
+ * @brief Z 传递函数
+ * @version 0.1
+ * @date 2023-06-26
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 #pragma once
 
 #include "discrete_tf.hpp"
 #include <vector>
 #include <stdexcept>
 
+/**
+ * @brief Z传递函数
+ *
+ * @tparam T 数据类型，例如 float 或 double
+ */
 template <typename T>
 class ZTf : public DiscreteTf<T>
 {
@@ -16,13 +32,31 @@ private:
     size_t index_ = 0;                // 上一次输入和输出在队列中的位置
 
 public:
+    /**
+     * @brief 创建空的 Z 传函
+     * @note 由于没有分子和分母，之后必须调用 Init() 指定分子和分母才能调用 Step()
+     */
     ZTf(){};
 
+    /**
+     * @brief 创建一个 Z 传函
+     *
+     * @param num 分子
+     * @param den 分母
+     * @note 分子阶数不能大于分母，否则是非因果系统
+     */
     ZTf(const std::vector<T> &num, const std::vector<T> &den)
     {
         Init(num, den);
     }
 
+    /**
+     * @brief 初始化 Z 传函或重新指定 Z 传函的表达式
+     *
+     * @param num 分子
+     * @param den 分母
+     * @note 分子阶数不能大于分母，否则是非因果系统
+     */
     void Init(const std::vector<T> &num, const std::vector<T> &den)
     {
         int size_diff = den.size() - num.size(); // 分母维数与分子维数之差
@@ -71,6 +105,12 @@ public:
         ResetState();
     }
 
+    /**
+     * @brief 走一个周期
+     *
+     * @param input 输入
+     * @return 输出
+     */
     T Step(T input) override
     {
         if (dim_ == 0) {
@@ -94,6 +134,10 @@ public:
         return output;
     }
 
+    /**
+     * @brief 重置内部状态
+     *
+     */
     void ResetState() override
     {
         std::fill(inputs_.begin(), inputs_.end(), 0);
